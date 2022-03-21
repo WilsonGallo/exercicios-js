759d0189a50b3377509bd4464970e07b55e899ca
Wilson Gallo <wilsongallo21@gmail.com>
2022-03-21 20:44:54 -0300

    Terceiro commit

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+//Código não executavel!!!
+
+
+// Procedural
+processamento(valo1, valor2, valor3)
+
+//OO
+objeto {
+    valor1;
+    valor2
+    valor3
+processamento()
+}
+
+objeto.processamento()// Foco passou
+
+
+//Princípios importantes:
+//1. Abstração
+//2. Encapsulamento
+//3. Herança (prototype)
+//4. Polimorfismo  - multiplas formas
